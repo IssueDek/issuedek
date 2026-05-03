9b5026e921d65344faa60aefd651e74295aece52
Mithun <sgmithun24@gmail.com>
2026-05-03 09:53:50 +0530

    postcss.config.js

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,15 @@
+/** @type {import('tailwindcss').Config} */
 module.exports = {
-  plugins: {
-    tailwindcss: {},
-    autoprefixer: {},
+  content: [
+    "./app/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [require("daisyui")],
+
+  daisyui: {
+    themes: ["light", "dark"],
   },
 };
