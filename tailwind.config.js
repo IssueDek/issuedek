017431fed2422014be49ee1d1db48afa8806471a
Mithun <sgmithun24@gmail.com>
2026-05-03 09:33:52 +0530

    CSS Update

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,9 +2,14 @@
 module.exports = {
   content: [
     "./app/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {},
   },
   plugins: [require("daisyui")],
+
+  daisyui: {
+    themes: ["light", "dark"],
+  },
 };
